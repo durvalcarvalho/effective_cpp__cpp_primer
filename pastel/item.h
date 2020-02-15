1682a4b233b364aa280705dfd8ee4743177dbde7
Durval Carvalho <durvalcsouza@outlook.com>
2020-02-15 11:09:03 -0300

    Add C++ OO

---
+++ pastel/item.h
@@ -0,0 +1,15 @@
+#ifndef ITEM_H
+#define ITEM_H
+
+#include <string>
+
+using namespace std;
+
+class Item {
+public:
+	virtual ~Item(){}
+	virtual string nome() const =0;
+	virtual float preco() const =0;
+};
+
+#endif
---
+++ pastel/pastel.cpp
@@ -0,0 +1,13 @@
+#include "pastel.h"
+
+const float Pastel::_preco = 2.50f;
+
+Pastel::Pastel(const string& sabor) : _sabor(sabor) {}
+
+string Pastel::nome() const {
+	return "Pastel de " + _sabor;
+}
+
+float Pastel::preco() const {
+	return _preco;
+}
