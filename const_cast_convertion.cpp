5e01c8283589eac4447d26cbc240cc96567d02b0
Durval Carvalho <durvalcsouza@outlook.com>
2020-02-15 10:16:46 -0300

    Add comments about const cast convertion

--- const_cast_convertion.cpp
+++ const_cast_convertion.cpp
@@ -11,6 +11,8 @@ private:
 	string text;
 	mutable size_t textLength;
 	mutable bool lengthIsValid;
+	// mutable type specifier means that this attribute can be modified on const methods
+
 public:
 	size_t length() const;
 
@@ -20,8 +22,12 @@ public:
 		return text[position];
 	}
 	
+	// this method is converting the this object to const to use this version of the const method
+	// this approach is useful for not repeating the code.
 	char& operator[](size_t position) {
 		return const_cast<char&>(static_cast<const TextBlock&>(*this)[position]);
+		//							^ converting this to const this
+		//                 ^ converting const char& to char&
 	}
 };
 
