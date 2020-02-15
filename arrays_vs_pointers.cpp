7cd92f2615419d9a5ffde7722656e919356272a1
Durval Carvalho <durvalcsouza@outlook.com>
2020-02-15 10:07:38 -0300

    Diff betwn char array and pointer of chars

--- arrays_vs_pointers.cpp
+++ arrays_vs_pointers.cpp
@@ -9,11 +9,16 @@ int main() {
 	// #1: char* - POINTER
 
 	// NÃO POSSO MUDAR A STRING, MAS POSSO MUDAR O PONTEIRO
-	const char* p1 = "Hello"; // PONTEIRO PARA CONST CHAR
-	// p1[0] = 'A';  		  // Segmentation fault
+	// PONTEIRO PARA CONST CHAR
+	const char* p1 = "Hello"; 
+
+	// Segmentation fault, pois estamos tentando mudar o char que é constante
+	// p1[0] = 'A';  		  
 
 	char chr = 'Z';
 	const char* p2 = "Hello";
+
+	// Eu posso mudar para onde o ponteiro aponta
 	p2 = &chr;
 
 
@@ -21,8 +26,8 @@ int main() {
 
 	// #2: char[] - ARRAY
 
-	// posso mudar os valores do array
-	char p3[] = "Hello";  // array de char
+	// posso mudar os valores do array, mas não posso mudar para onde o array aponta
+	char p3[] = "Hello";
 	p3[0] = 'B';
 	// cout << p3 << endl;
 
@@ -31,7 +36,6 @@ int main() {
 	char p4[] = "Hello";
 	char chz = 'Z';
 	p4 = &chz;
-
 
 	return 0;
 }
